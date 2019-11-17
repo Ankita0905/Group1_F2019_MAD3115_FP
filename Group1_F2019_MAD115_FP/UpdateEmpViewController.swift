dae3d4b7538ccbefdc4098f4cb33642911c24baa
Karan <kumarkay.ca@gmail.com>
2019-11-17 14:00:40 -0500

    fields connected

--- Group1_F2019_MAD115_FP/UpdateEmpViewController.swift
+++ Group1_F2019_MAD115_FP/UpdateEmpViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class UpdateEmpViewController: UIViewController {
 
+    @IBOutlet weak var txtUpdateFName: UITextField!
+    @IBOutlet weak var txtUpdateLName: UITextField!
+    @IBOutlet weak var txtUpdateEmail: UITextField!
+    @IBOutlet weak var txtUpdateDOB: UITextField!
+    @IBOutlet weak var segUpdateEmpType: UISegmentedControl!
+    
+    @IBOutlet weak var txtUpdateSchoolName: UITextField!
+    
+    @IBOutlet weak var txtUpdateSalary: UITextField!
+    
+    @IBOutlet weak var txtUpdateBonus: UITextField!
+    @IBOutlet weak var segUpdatePartTimeType: UISegmentedControl!
+    
+    @IBOutlet weak var txtUpdateRate: UITextField!
+    @IBOutlet weak var txtUpdateHrsWorked: UITextField!
+    
+    @IBOutlet weak var txtUpdateCommission: UITextField!
+    
+    @IBOutlet weak var txtUpdateAmount: UITextField!
+    
+    @IBOutlet weak var updateInterView: UIView!
+    @IBOutlet weak var updateFullTimeView: UIView!
+    @IBOutlet weak var updatePartTimeView: UIView!
+    @IBOutlet weak var updateCommissionView: UIView!
+    
+    @IBOutlet weak var updateFixedView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
        
     }
     
